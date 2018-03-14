5fbc914aa528c13e889fb9a82261f680fa3fbe78
ssimon9 <ssimon9@mail.depaul.edu>
2018-03-14 17:52:08 -0400

    final

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   include CurrentCart
-  before_action :set_cart only: [:create]
+  before_action :set_cart, only: [:create]
   
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
@@ -24,17 +24,18 @@ class LineItemsController < ApplicationController
   def edit
   end
 
-  # POST /line_items
-  # POST /line_items.json
+#...
+
+        
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.add_products(product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        #format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
-      else
+    else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
       end
@@ -73,6 +74,6 @@ class LineItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
 end
